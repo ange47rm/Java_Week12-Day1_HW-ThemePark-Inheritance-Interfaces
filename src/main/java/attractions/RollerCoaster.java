119a83aef6c6b548ae334d020e4fed0ae4b7c446
ange47rm <ange47rm@hotmail.com>
2021-02-15 18:16:40 +0000

    Implemented double price on Rollercoaster for tall people, and tested

--- src/main/java/attractions/RollerCoaster.java
+++ src/main/java/attractions/RollerCoaster.java
@@ -1,9 +1,10 @@
 package attractions;
 
 import interfaces.ISecurity;
+import interfaces.ITicketed;
 import people.Visitor;
 
-public class RollerCoaster extends Attraction implements ISecurity {
+public class RollerCoaster extends Attraction implements ISecurity, ITicketed {
 
     public RollerCoaster(String name, int rating) {
         super(name, rating);
@@ -15,6 +16,19 @@ public class RollerCoaster extends Attraction implements ISecurity {
             return true;
         } else {
             return false;
+        }
+    }
+
+    @Override
+    public double defaultPrice() {
+        return 8.40;
+    }
+
+    public double priceFor(Visitor visitor){
+        if (visitor.getHeight() > 200) {
+            return defaultPrice() * 2;
+        } else {
+            return defaultPrice();
         }
     }
 }
--- src/test/java/attractions/RollercoasterTest.java
+++ src/test/java/attractions/RollercoasterTest.java
@@ -3,6 +3,8 @@ package attractions;
 import org.junit.Before;
 import org.junit.Test;
 
+import people.Visitor;
+
 import static org.junit.Assert.assertEquals;
 
 public class RollercoasterTest {
@@ -27,5 +29,16 @@ public class RollercoasterTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, rollerCoaster.getVisitCount());
+    }
+
+    @Test
+    public void returnsDefaultPrice(){
+        assertEquals(8.40, rollerCoaster.defaultPrice(), 0.0);
+    }
+
+    @Test
+    public void returnsDoublePriceForTallPeople(){
+        Visitor tallBoy = new Visitor(20, 205.00, 20.00);
+        assertEquals(16.80, rollerCoaster.priceFor(tallBoy), 0.0);
     }
 }

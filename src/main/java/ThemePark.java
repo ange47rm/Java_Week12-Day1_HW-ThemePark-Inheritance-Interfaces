405de1d5a08621192c2b24c98f271d46fb65072e
ange47rm <ange47rm@hotmail.com>
2021-02-16 10:14:36 +0000

    Passed ThemePark getAllReviewed test

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,2 +1,40 @@
+import attractions.Attraction;
+import interfaces.IReviewed;
+import stalls.Stall;
+
+import java.util.ArrayList;
+
 public class ThemePark {
+
+    public ArrayList<Attraction> attractions;
+    private ArrayList<Stall> stalls;
+
+    public ThemePark (ArrayList<Attraction> attractions, ArrayList<Stall> stalls ){
+        this.attractions = new ArrayList<Attraction>();
+        this.stalls = new ArrayList<Stall>();
+    }
+
+    public void addAttraction(Attraction attraction){
+        this.attractions.add(attraction);
+    }
+
+    public void addStall(Stall stall){
+        this.stalls.add(stall);
+    }
+
+    public int getAttractionCount(){
+        return this.attractions.size();
+    }
+
+    public int getStallCount(){
+        return this.stalls.size();
+    }
+
+    public ArrayList<IReviewed> getAllReviewed(){
+        ArrayList<IReviewed> allReviewed;
+        allReviewed = new ArrayList<IReviewed>();
+        allReviewed.addAll(this.attractions);
+        allReviewed.addAll(this.stalls);
+        return allReviewed;
+    }
 }
--- src/test/java/stalls/TobaccoStallTest.java
+++ src/test/java/stalls/TobaccoStallTest.java
@@ -2,6 +2,7 @@ package stalls;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
@@ -28,4 +29,18 @@ public class TobaccoStallTest {
     public void hasParkingSpot() {
         assertEquals(ParkingSpot.B1, tobaccoStall.getParkingSpot());
     }
+
+    @Test
+    public void visitorNotAllowed(){
+        Visitor youngBoy = new Visitor(14, 167.50, 13.50);
+        assertEquals(false, tobaccoStall.isAllowedTo(youngBoy));
+    }
+
+    @Test
+    public void visitorAllowed(){
+        Visitor oldBoy = new Visitor(29, 178.30, 19.20);
+        assertEquals(true, tobaccoStall.isAllowedTo(oldBoy));
+    }
+
+
 }
